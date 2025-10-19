5e4f44bb679089251f8c590682196cba111d7a3b
Владислав Салтанов <saltanov@sfedu.ru>
2025-10-19 21:02:53 +0300

    fine tuning movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -17,40 +17,42 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
         InputManager inputManager;
 
-
+    bool isForcedFalling;
     private void Start()
     {
         inputManager.jumpAction.action.performed += Jump;
     }
 
-    private void OnDisable()
+    private void OnDestroy()
     {
         inputManager.jumpAction.action.performed -= Jump;
     }
 
     private void Update()
     {
+        isForcedFalling = inputManager.moveAction.action.ReadValue<Vector2>().y < -0.5f && (controller.currentTime - controller.lastGroundedTime > chars.forcedFallCooldown);
+
         // resetting speed if we stop moving
         if (Mathf.Abs(inputManager.moveAction.action.ReadValue<Vector2>().x) < 0.1f && isGroundedHandler.isGrounded)
             rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, 0f, chars.resetSpeedTime * Time.deltaTime);
 
         // clamping velocity so we wont fly too fast
         rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -chars.maxHorizontalSpeed, chars.maxHorizontalSpeed);
-        rb.linearVelocityY = Mathf.Clamp(rb.linearVelocityY, chars.maxVerticalSpeed.x, chars.maxVerticalSpeed.y);
+        rb.linearVelocityY = Mathf.Clamp(rb.linearVelocityY, isForcedFalling ? -chars.forcedFallForce : chars.maxVerticalSpeed.x, chars.maxVerticalSpeed.y);
+
+        // horizontal movement
+        if (Mathf.Abs(inputManager.moveAction.action.ReadValue<Vector2>().x) > 0)
+            rb.linearVelocityX += inputManager.moveAction.action.ReadValue<Vector2>().x * chars.speed;
     }
 
     private void FixedUpdate()
     {
-        // horizontal movement
-        if (Mathf.Abs(inputManager.moveAction.action.ReadValue<Vector2>().x) > 0)
-            rb.linearVelocityX += inputManager.moveAction.action.ReadValue<Vector2>().x * chars.speed;
-
         // holding down 's' or stick down makes player slam down (after cooldown)
-        if (inputManager.moveAction.action.ReadValue<Vector2>().y < -0.5f && (controller.currentTime - controller.lastGroundedTime > chars.forcedFallCooldown))
+        if (isForcedFalling)
             rb.AddForce(Vector2.down * chars.forcedFallForce, ForceMode2D.Force);
 
         // additional gravity if player is falling down
-        if (rb.linearVelocityY < 0.5f)
+        if (rb.linearVelocityY < 0f)
             rb.AddForce(Vector2.down * chars.additionalGravity, ForceMode2D.Force);
     }
 
@@ -59,7 +61,7 @@ public class PlayerMovement : MonoBehaviour
         // TODO: add coyote time
         if (callback.performed && isGroundedHandler.isGrounded)
         {
-            rb.linearVelocityY = 0;
+            rb.linearVelocityY = 2;
             rb.AddForce(Vector2.up * chars.jumpForce, ForceMode2D.Impulse);
         }
     }
