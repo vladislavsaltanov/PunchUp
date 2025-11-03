ebaee0b5960bc73e6158245c89e39d2deb63f849
Владислав Салтанов <saltanov@sfedu.ru>
2025-11-03 22:29:40 +0300

    Added more movement mechanics
    Added ability to jump n times, added buffered jump, added coyote time. Fine-tuned movement values.

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField]
-        MovementCharacteristics chars;
+    MovementCharacteristics chars;
 
     [Space(20)]
 
@@ -18,18 +18,30 @@ public class PlayerMovement : MonoBehaviour
         InputManager inputManager;
 
     bool isForcedFalling;
+    byte jumpsRemaining;
+    float coyoteTimeTimerCurrent, bufferedJumpTimerCurrent, jumpCooldown = -0.1f;
+
+    [HideInInspector]
+        float movementDirection;
+
     private void Start()
     {
-        inputManager.jumpAction.action.performed += Jump;
+        inputManager.jumpAction.action.performed += JumpAction;
+        coyoteTimeTimerCurrent = chars.coyoteTime;
     }
 
     private void OnDestroy()
     {
-        inputManager.jumpAction.action.performed -= Jump;
+        inputManager.jumpAction.action.performed -= JumpAction;
     }
 
     private void Update()
     {
+        // -1 if going left, 1 if going right
+        movementDirection = inputManager.moveAction.action.ReadValue<Vector2>().x == 0f ? 0 : inputManager.moveAction.action.ReadValue<Vector2>().x > 0.15f ? 1 : inputManager.moveAction.action.ReadValue<Vector2>().x <= 0.15f ? -1 : 0;
+
+        rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, movementDirection * chars.speed, chars.resetSpeedTime * Time.deltaTime);
+
         isForcedFalling = inputManager.moveAction.action.ReadValue<Vector2>().y < -0.5f && (controller.currentTime - controller.lastGroundedTime > chars.forcedFallCooldown);
 
         // resetting speed if we stop moving
@@ -37,12 +49,23 @@ public class PlayerMovement : MonoBehaviour
             rb.linearVelocityX = Mathf.Lerp(rb.linearVelocityX, 0f, chars.resetSpeedTime * Time.deltaTime);
 
         // clamping velocity so we wont fly too fast
-        rb.linearVelocityX = Mathf.Clamp(rb.linearVelocityX, -chars.maxHorizontalSpeed, chars.maxHorizontalSpeed);
         rb.linearVelocityY = Mathf.Clamp(rb.linearVelocityY, isForcedFalling ? -chars.forcedFallForce : chars.maxVerticalSpeed.x, chars.maxVerticalSpeed.y);
 
-        // horizontal movement
-        if (Mathf.Abs(inputManager.moveAction.action.ReadValue<Vector2>().x) > 0)
-            rb.linearVelocityX += inputManager.moveAction.action.ReadValue<Vector2>().x * chars.speed;
+        if (isGroundedHandler.isGrounded)
+        {
+            jumpsRemaining = chars.maxJumps;
+            coyoteTimeTimerCurrent = chars.coyoteTime;
+        }
+        else
+            coyoteTimeTimerCurrent -= Time.deltaTime;
+
+        if (bufferedJumpTimerCurrent > 0f)
+            bufferedJumpTimerCurrent -= Time.deltaTime;
+
+        jumpCooldown -= Time.deltaTime;
+
+        if ((coyoteTimeTimerCurrent > 0f && bufferedJumpTimerCurrent > 0f) || (jumpsRemaining > 0 && jumpCooldown > 0 && bufferedJumpTimerCurrent > 0f))
+            Jump();
     }
 
     private void FixedUpdate()
@@ -56,14 +79,23 @@ public class PlayerMovement : MonoBehaviour
             rb.AddForce(Vector2.down * chars.additionalGravity, ForceMode2D.Force);
     }
 
-    void Jump(InputAction.CallbackContext callback)
+    void JumpAction(InputAction.CallbackContext callback)
     {
-        // TODO: add coyote time
-        if (callback.performed && isGroundedHandler.isGrounded)
+        if (callback.performed && jumpsRemaining > 0)
         {
-            rb.linearVelocityY = 2;
-            rb.AddForce(Vector2.up * chars.jumpForce, ForceMode2D.Impulse);
+            bufferedJumpTimerCurrent = chars.bufferedJumpTimer;
+            jumpCooldown = chars.jumpCooldown;
+            jumpsRemaining--;
         }
+    }
+
+    void Jump()
+    {
+        coyoteTimeTimerCurrent = 0f;
+        bufferedJumpTimerCurrent = 0f;
+
+        rb.linearVelocityY = 0;
+        rb.AddForce(Vector2.up * chars.jumpForce, ForceMode2D.Impulse);
     }
 }
 [System.Serializable]
@@ -76,4 +108,6 @@ public class MovementCharacteristics
     public Vector2 maxVerticalSpeed;
     public float additionalGravity;
     public float forcedFallCooldown, forcedFallForce;
+    public float coyoteTime = 0.25f, bufferedJumpTimer = 0.2f, jumpCooldown = 0.25f;
+    public byte maxJumps = 1;
 }
