56e65d563c0d3b6509c1ca19a47338ce9d6cbf13
Dron03127 <sorochi@sfedu.ru>
2025-12-03 19:07:55 +0300

    Changed CameraZone trigger scripts

--- Assets/Scripts/Camera/CameraZoneTriggerX.cs
+++ Assets/Scripts/Camera/CameraZoneTriggerX.cs
@@ -9,6 +9,7 @@ public class CameraZone : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D other)
     {
+
         float exitDirection = other.transform.position.x - transform.position.x;
         CinemachineCamera targetCamera = exitDirection > 0 ? rightExitCamera : leftExitCamera;
 
