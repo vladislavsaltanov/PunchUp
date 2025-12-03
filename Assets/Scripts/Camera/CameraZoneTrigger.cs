5074fb311513390e1060abe22c3be2fd5620443e
Dron03127 <sorochi@sfedu.ru>
2025-12-03 23:03:28 +0300

    New layers

--- Assets/Scripts/Camera/CameraZoneTrigger.cs
+++ Assets/Scripts/Camera/CameraZoneTrigger.cs
@@ -3,29 +3,12 @@ using Unity.Cinemachine;
 
 public class CameraZone : MonoBehaviour
 {
-    [Header("Camera Settings")]
-    [SerializeField] private CinemachineCamera Camera;
-    public Transform Room = null;
+    [Header("Confiner Settings")]
+    [SerializeField] private BoxCollider2D targetConfiner;
+
     private void OnTriggerExit2D(Collider2D tr)
     {
-        Room = FloorManager.Instance.pathwaysPlaceholders[FloorManager.Instance.EnterInd];
-        Camera = CameraManager.Instance.GetCamera();
-        float Xpos = tr.transform.position.x - Room.position.x;
-        float Ypos = tr.transform.position.y - Room.position.y;
-        if (Mathf.Abs(Xpos) > 28)
-        {
-            //GetComponent<CameraManager>().MoveCameraTarget(Room.position.x + 40, Room.position.y);
-            FloorManager.Instance.currentRoom.position.Set(Room.position.x + 40, Room.position.y, Room.position.z);
-        }
-        else
-        {
-            //GetComponent<CameraManager>().MoveCameraTarget(Room.position.x, Room.position.y + 20);
-            FloorManager.Instance.currentRoom.position.Set(Room.position.x, Room.position.y + 20, Room.position.z);
-        }
-    }
-
-    public void SetStartRoom(Transform pos)
-    {
-       Room = pos;
+        Debug.Log(tr.name);
+        targetConfiner.transform.position += new Vector3(20, 0, 0);
     }
 }
