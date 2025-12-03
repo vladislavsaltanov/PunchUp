5ed407a6dfa5f842a4f62978099e9a08de4fdeca
Zur-0 <Egor_Med@outlook.com>
2025-12-03 17:26:17 +0300

    Creating floor generation (#8)
    * Squashed commit of the following:
    
    commit eb3988e8e83109f1bf9b5cb71c469e5c1c876a0e
    Author: Владислав Салтанов <saltanov@sfedu.ru>
    Date:   Tue Nov 4 01:01:46 2025 +0300
    
        test
    
    * Added room placeholders
    
    * Add room generator
    
    * Add pathways base
    
    * Finish Floor generator
    
    * Add room manager script
    
    * Add 3 Rooms template
    
    * Finish floor generation
    
    * Add elevator generating
    
    * Add enemy placeholders and update RoomManager
    
    ---------
    
    Co-authored-by: Владислав Салтанов <saltanov@sfedu.ru>

---
+++ Assets/Scripts/FloorGeneration/FloorManager.cs
@@ -0,0 +1,70 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class FloorManager : MonoBehaviour
+{
+    List<GameObject> currentRooms = new List<GameObject>();
+    public List<Transform> placeholders;
+    public List<GameObject> roomPrefabs;
+    List<GameObject> _roomPrefabs;
+    public List<Transform> pathwaysPlaceholders;
+    public List<Transform> blockPlaceholders;
+    public List<GameObject> pathwaysPrefabs;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartGenerating();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateRoom(int i)
+    {
+        int randomIndex = Random.Range(0, _roomPrefabs.Count);
+        GameObject selectedRoom = _roomPrefabs[randomIndex];
+
+        currentRooms.Add(Instantiate(selectedRoom, placeholders[i]));
+        currentRooms[i].GetComponent<RoomManager>().SetRoomID(i);
+        _roomPrefabs.RemoveAt(randomIndex);
+    }
+
+    void GeneratePathways()
+    {
+        for (int i = 0; i < 23; i += 2)
+        {
+            int randomIndex = Random.Range(0,2);
+            Instantiate(pathwaysPrefabs[1], pathwaysPlaceholders[i + randomIndex]);
+            Instantiate(pathwaysPrefabs[0], pathwaysPlaceholders[i + 1 - randomIndex]);
+        }
+    }
+
+    void StartGenerating()
+    {
+        _roomPrefabs = new List<GameObject>(roomPrefabs);
+        int EnterInd = Random.Range(0, 3);
+        int ExitInd = Random.Range(3, 9);
+
+        //Генерация комнат
+        for (int i = 0; i < 9; i++)
+        {
+            GenerateRoom(i);
+        }
+        //Спавн лифтов
+        currentRooms[EnterInd].GetComponent<RoomManager>().InitializeElevator(0);
+        currentRooms[ExitInd].GetComponent<RoomManager>().InitializeElevator(1);
+
+        //Спавн "Блоков" на границах уровня
+        for (int i = 0; i < 24; i++)
+        {
+            Instantiate(pathwaysPrefabs[2], blockPlaceholders[i]);
+        }
+        //Генерация проходов
+        GeneratePathways();
+    }
+}
---
+++ Assets/Scripts/Room/RoomManager.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public int roomID;
+    public Transform elevatorPlaceholder;
+    public List<GameObject> elevators;
+    public Transform playerSpawnPosition;
+    public List<Transform> roomObjectsPlaceholders;
+    public List<Transform> enemyPositions;
+    // Audio bank
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitializeElevator(int i)
+    {
+        Instantiate(elevators[i], elevatorPlaceholder);
+    }
+
+    public void SetRoomID(int i)
+    {
+        roomID = i;
+    }
+}
